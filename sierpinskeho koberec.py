ae2f51f5219ed05b74a802cae16f961050b172e3
pavlickosamuel <pavlickosamuel@gmail.com>
2026-09-21 13:57:32 +0200

    druhy

--- sierpinskeho koberec.py
+++ sierpinskeho koberec.py
@@ -7,7 +7,7 @@ canvas.pack()
 def draw_carpet(a, x, y):
     if a > 10:
         canvas.create_rectangle(x, y, x + a, y+a, outline = "black")
-        draw_carpet(a / 3 , x + a / 3  , y + a / 3 )
+        draw_carpet(a , x + a / 3, y)
         
 draw_carpet (900, 50, 50)
 window.mainloop()
